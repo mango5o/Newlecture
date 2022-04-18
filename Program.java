edfe76dbf096421872b83c4e722289da3e3aaf3f
mango5o <85277818+mango5o@users.noreply.github.com>
2022-04-18 23:19:05 +0900

    ex01/io/Program
    ex01번에 추가하는 입출력 관련 JAVA 파일

---
+++ Program.java
@@ -0,0 +1,14 @@
+package ex01.io;
+// 16강 입출력 관련 java 파일
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.write(3); // 코드값만 입력한 예시
+		System.out.write('3'); // 3이라는 기호를 입력한 예시
+		
+		System.out.flush();
+	}
+	
+
+}
